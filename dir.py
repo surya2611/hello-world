fb4b240091b48ef2dd7e1e4106bcfb94824a5c50
surya <surya.v.sainadh@gmail.com>
2019-02-12 23:42:00 +0530

    code modified and developed for case of no parent directory or whose parent directory is drive

--- dir.py
+++ dir.py
@@ -5,35 +5,30 @@
 
 import os, time, shutil
 
-file = [os.getcwd(), os.path.abspath(os.pardir)]
-
 output = []
-output.append('\n Directory of ' + file[0] + '\n')
+output.append('\n Directory of ' + os.getcwd() + '\n')
+if os.path.abspath(os.pardir) != os.path.abspath(os.sep):
+    file = [os.curdir, os.pardir]
 
-total_size_of_files = no_of_directories = 0
-name = ''
 
 for i in os.listdir('.'):
     if i.startswith('.'):
         continue
-    file.append(os.path.realpath(i))
+    file.append(i)
+
+total_size_of_files = no_of_directories = 0
+name = ''
 
 for j in file:
-
-    last_mtim = time.localtime(os.path.getmtime(j))
+    path = os.path.abspath(j)
+    if j == os.pardir:
+        path = os.getcwd()
+    last_mtim = time.localtime(os.path.getmtime(path))
     f_time = time.strftime('%d-%m-%Y  %H:%M', last_mtim)
 
     if os.path.isdir(j):
         no_of_directories += 1
-        if j == os.getcwd():
-            name = '.'
-            cwd_last_mtime = f_time
-        elif j == os.path.abspath(os.pardir):
-            name = '..'
-            f_time = cwd_last_mtime
-        else:
-            name = os.path.basename(j)
-        output.append('{:<20} {:<15}{}'.format(f_time, '(DIR)', name))
+        output.append('{:<20} {:<15}{}'.format(f_time, '(DIR)', j))
     else:
         total_size_of_files += os.path.getsize(j)
         output.append('{:<20} {:>14,} {}'.format(f_time, os.path.getsize(j), os.path.basename(j)))
