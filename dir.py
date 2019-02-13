7a1554b74dc8d47cf6e6a9b5d518c9291930b4ef
surya <surya.v.sainadh@gmail.com>
2019-02-14 01:09:24 +0530

    collected all data in collect_data dictionary

--- dir.py
+++ dir.py
@@ -12,29 +12,27 @@ file_order = '{}{:>18} {}'
 print_data.append('\n Directory of ' + os.getcwd() + '\n')
 
 if os.path.abspath(os.pardir) != os.path.abspath(os.sep):
-    file = [os.curdir, os.pardir]
+    collect_data['file'] = [os.curdir, os.pardir]
 
 for i in os.listdir('.'):
     if i.startswith('.'):
         continue
-    file.append(i)
+    collect_data['file'].append(i)
 
-collect_data['file'] = file
+collect_data['no_of_directories'] = 0
 
-no_of_directories = 0
-
-for j in file:
-    path = (os.path.abspath(j))
+for j in collect_data['file']:
+    path = os.path.abspath(j)
     collect_data['path'].append(path)
     if j == os.pardir:
         path = os.getcwd()
     last_mtim = time.localtime(os.path.getmtime(path))
-    last_mod_time = (time.strftime('%d-%m-%Y  %H:%M', last_mtim))
+    last_mod_time = time.strftime('%d-%m-%Y  %H:%M', last_mtim)
     collect_data['last_mod_time'].append(last_mod_time)
 
     if os.path.isdir(j):
         collect_data['is_directory'].append(True)
-        no_of_directories += 1
+        collect_data['no_of_directories'] += 1
         collect_data['file_size'].append(0)
         print_data.append(dir_order.format(last_mod_time, '(DIR)', j))
     else:
@@ -43,11 +41,12 @@ for j in file:
         collect_data['file_size'].append(file_size)
         print_data.append(file_order.format(last_mod_time, file_size, j))
 
-no_of_files = len(file) - no_of_directories
-total_size_of_files = sum(collect_data['file_size'])
+collect_data['no_of_files'] = len(collect_data['file']) - collect_data['no_of_directories']
+collect_data['total_size_of_files'] = sum(collect_data['file_size'])
+collect_data['free_vol_in_drive'] = shutil.disk_usage('.').free
 
-print_data.append('{:>16,} File(s){:>15,} bytes'.format(no_of_files, total_size_of_files))
-print_data.append('{:>16,} Dir(s){:>16,} bytes free'.format(no_of_directories, shutil.disk_usage('.').free))
+print_data.append('{:>16,} File(s){:>15,} bytes'.format(collect_data['no_of_files'], collect_data['total_size_of_files']))
+print_data.append('{:>16,} Dir(s){:>16,} bytes free'.format(collect_data['no_of_directories'], collect_data['free_vol_in_drive']))
 
 for i in print_data:
     print(i)
