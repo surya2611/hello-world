46d868c7c0d918ac300ceb31f0e44d295093c410
surya <surya.v.sainadh@gmail.com>
2019-02-14 13:15:22 +0530

    collect_data format is modified

--- dir.py
+++ dir.py
@@ -4,7 +4,7 @@
 
 import os, time, shutil
 
-collect_data = {'file': [], 'path': [], 'last_mod_time': [], 'is_directory': [], 'file_size': []}
+collect_data = []
 print_data = []
 dir_order = '{:<21}{:<15}{}'
 file_order = '{}{:>18} {}'
@@ -12,41 +12,44 @@ file_order = '{}{:>18} {}'
 print_data.append('\n Directory of ' + os.getcwd() + '\n')
 
 if os.path.abspath(os.pardir) != os.path.abspath(os.sep):
-    collect_data['file'] = [os.curdir, os.pardir]
+    collect_data = [{'file': os.curdir}, {'file': os.pardir}]
 
 for i in os.listdir('.'):
     if i.startswith('.'):
         continue
-    collect_data['file'].append(i)
+    collect_data.append({'file': i})
 
-collect_data['no_of_directories'] = 0
+no_of_directories = 0
+total_size_of_files = 0
 
-for j in collect_data['file']:
-    path = os.path.abspath(j)
-    collect_data['path'].append(path)
-    if j == os.pardir:
+for j in collect_data:
+    path = os.path.abspath(j['file'])
+    if j['file'] == os.pardir:
         path = os.getcwd()
     last_mtim = time.localtime(os.path.getmtime(path))
-    last_mod_time = time.strftime('%d-%m-%Y  %H:%M', last_mtim)
-    collect_data['last_mod_time'].append(last_mod_time)
+    j['last_mod_time'] = time.strftime('%d-%m-%Y  %H:%M', last_mtim)
 
-    if os.path.isdir(j):
-        collect_data['is_directory'].append(True)
-        collect_data['no_of_directories'] += 1
-        collect_data['file_size'].append(0)
-        print_data.append(dir_order.format(last_mod_time, '(DIR)', j))
+    if os.path.isdir(j['file']):
+        j['is_directory'] = True
+        no_of_directories += 1
+        print_data.append(dir_order.format(j['last_mod_time'], '(DIR)', j['file']))
     else:
-        collect_data['is_directory'].append(False)
-        file_size = os.path.getsize(path)
-        collect_data['file_size'].append(file_size)
-        print_data.append(file_order.format(last_mod_time, file_size, j))
+        j['is_directory'] = False
+        j['file_size'] = os.path.getsize(path)
+        total_size_of_files += j['file_size']
+        print_data.append(file_order.format(j['last_mod_time'], j['file_size'], j['file']))
+    j['path'] = path
 
-collect_data['no_of_files'] = len(collect_data['file']) - collect_data['no_of_directories']
-collect_data['total_size_of_files'] = sum(collect_data['file_size'])
-collect_data['free_vol_in_drive'] = shutil.disk_usage('.').free
+no_of_files = len(collect_data) - no_of_directories
+free_vol_in_drive = shutil.disk_usage('.').free
+collect_data.append({'no_of_files': no_of_files,
+                     'no_of_directories': no_of_directories,
+                     'total_size_of_files': total_size_of_files,
+                     'free_vol_in_drive': free_vol_in_drive
+                     })
 
-print_data.append('{:>16,} File(s){:>15,} bytes'.format(collect_data['no_of_files'], collect_data['total_size_of_files']))
-print_data.append('{:>16,} Dir(s){:>16,} bytes free'.format(collect_data['no_of_directories'], collect_data['free_vol_in_drive']))
+print_data.append('{:>16,} File(s){:>15,} bytes'.format(no_of_files, total_size_of_files))
+print_data.append('{:>16,} Dir(s){:>16,} bytes free'.format(no_of_directories, free_vol_in_drive))
 
 for i in print_data:
     print(i)
