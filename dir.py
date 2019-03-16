ca9f7a6fb3c9fad75c566863f6839c590d3abac3
surya <surya.v.sainadh@gmail.com>
2019-03-16 22:20:21 +0530

    implemented to get data of specified sub directories/files using command line arguments

--- dir.py
+++ dir.py
@@ -1,55 +1,78 @@
 # TODO LIST
+#implement first 2 lines of the program
 #implement collect_data and print_data functions
-import os, time, shutil
+import os, time, shutil, sys
 
 class Path:
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path
 
-    def name(self, path):
-        return os.path.basename(path)
+    def name(self):
+        return os.path.basename(self.path)
 
-    def last_modified_time(self, path):
-        return time.strftime('%d-%m-%Y  %H:%M', time.localtime(os.path.getmtime(path)))
+    def last_modified_time(self):
+        return time.strftime('%d-%m-%Y  %H:%M', time.localtime(os.path.getmtime(self.path)))
 
-    def is_directory(self, path):
-        return os.path.isdir(path)
+    def is_directory(self):
+        return os.path.isdir(self.path)
 
-    def is_file(self, path):
-        return os.path.isfile(path)
+    def is_file(self):
+        return os.path.isfile(self.path)
 
     def files(self):
-        return len([i for i in os.listdir('.') if not i.startswith('.') and self.is_file(i)])
+        return [i for i in self.children() if Path(i).is_file()]
 
     def directories(self):
-        return len([i for i in os.listdir('.') if not i.startswith('.') and self.is_directory(i)]) + 2
+        return [i for i in self.children() if Path(i).is_directory()]
 
-    def size(self, path):
-        if self.is_file(path):
-            return os.stat(path).st_size
+    def size(self):
+        if self.is_file():
+            return os.stat(self.path).st_size
 
     def children(self):
-        return [i for i in os.listdir('.') if not i.startswith('.')]
+        if self.is_directory():
+            return [i for i in os.listdir(self.path) if not i.startswith('.')]
 
     def parent(self):
-        return [i for i in os.path.abspath('.').split('\\')]
+        return os.pardir
 
-cwd = Path()
-free_vol_in_drive = shutil.disk_usage('.').free
-total_size_of_files = sum([cwd.size(i) for i in cwd.children() if cwd.is_file(i)])
-def print_cwd_data():
+cwd = Path('.')
+
+if len(sys.argv) is 2:
+    if os.path.isdir('./' + sys.argv[1]):
+        os.chdir('./' + sys.argv[1])
+    else:
+        cwd = Path('./' + sys.argv[1])
+
+cwd.free_vol_in_drive = shutil.disk_usage('.').free
+if cwd.is_directory():
+    cwd.no_of_files = len(cwd.files())
+    cwd.total_size_of_files = sum([Path(file).size() for file in cwd.files()])
+    if cwd.parent():
+        cwd.no_of_directories = len(cwd.directories()) + 2
+    else:
+        cwd.no_of_directories = len(cwd.directories())
+else:
+    cwd.no_of_files = 1
+    cwd.total_size_of_files = cwd.size()
+    cwd.no_of_directories = 0
+
+def print_dir_data(cwd):
     dir_order = '{:<21}{:<15}{}'
     file_order = '{}{:>18,} {}'
     print('\n Directory of ' + os.getcwd() + '\n')
-    if len(cwd.parent()) > 1:
-        print(dir_order.format(cwd.last_modified_time('.'), '(DIR)', '.'))
-        print(dir_order.format(cwd.last_modified_time('.'), '(DIR)', '..'))
-    for i in cwd.children():
-        if cwd.is_directory(i):
-            print(dir_order.format(cwd.last_modified_time(i), '(DIR)', cwd.name(i)))
-        else:
-            print(file_order.format(cwd.last_modified_time(i), cwd.size(i), cwd.name(i)))
-    print('{:>16,} File(s){:>15,} bytes'.format(cwd.files(), total_size_of_files))
-    print('{:>16,} Dir(s){:>16,} bytes free'.format(cwd.directories(), free_vol_in_drive))
+    if cwd.is_directory():
+        if cwd.parent():
+            print(dir_order.format(cwd.last_modified_time(), '<DIR>', cwd.name()))
+            print(dir_order.format(cwd.last_modified_time(), '<DIR>', cwd.parent()))
+        for i in cwd.children():
+            if Path(i).is_directory():
+                print(dir_order.format(Path(i).last_modified_time(), '<DIR>', Path(i).name()))
+            else:
+                print(file_order.format(Path(i).last_modified_time(), Path(i).size(), Path(i).name()))
+    else:
+        print(file_order.format(cwd.last_modified_time(), cwd.size(), cwd.name()))
+    print('{:>16,} File(s){:>15,} bytes'.format(cwd.no_of_files, cwd.total_size_of_files))
+    print('{:>16,} Dir(s){:>16,} bytes free'.format(cwd.no_of_directories, cwd.free_vol_in_drive))
 
-print(print_cwd_data())
+print_dir_data(cwd)
